1d9b3c049355385da6d875ebdd96839b34b8b960
Delfino <99680637+GuilhermeDelfino@users.noreply.github.com>
2022-11-15 15:33:16 -0300

    Update tables-mssql.sql

--- tables-mssql.sql
+++ tables-mssql.sql
@@ -1,135 +1,86 @@
--- DROP SCHEMA dbo;
-
-CREATE SCHEMA dbo;
--- datasentry.dbo.ComponentType definition
-
--- Drop table
-
--- DROP TABLE datasentry.dbo.ComponentType;
-
-CREATE TABLE ComponentType (
-	[_idComponentType] int IDENTITY(1,1) NOT NULL,
-	description varchar(25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	measuramentUnit varchar(5) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	createdAt datetime DEFAULT getdate() NULL,
-	updatedAt datetime DEFAULT getdate() NULL,
-	CONSTRAINT PK__Componen__37C058D58D4F0459 PRIMARY KEY ([_idComponentType])
+CREATE DATABASE datasentry;
+GO;
+USE datasentry;
+GO;
+CREATE TABLE Hospital(
+	_idHospital INT PRIMARY KEY IDENTITY(1,1),
+	cnpj CHAR(14),
+	cep CHAR(8),
+	numberAddress VARCHAR(10),
+	complement VARCHAR(50),
+	fantasyName VARCHAR(50),
+	unit VARCHAR(50),
+	corporateName VARCHAR(50),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP 
 );
-
-
--- datasentry.dbo.Hospital definition
-
--- Drop table
-
--- DROP TABLE datasentry.dbo.Hospital;
-
-CREATE TABLE Hospital (
-	[_idHospital] int IDENTITY(1,1) NOT NULL,
-	cnpj char(14) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	cep char(8) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	numberAddress varchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	complement varchar(25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	fantasyName varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	unit varchar(25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	corporateName varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	createdAt datetime DEFAULT getdate() NULL,
-	updatedAt datetime DEFAULT getdate() NULL,
-	CONSTRAINT PK__Hospital__3191C4651C725789 PRIMARY KEY ([_idHospital])
+GO;
+CREATE TABLE UserHospital(
+	_idUserHospital INT PRIMARY KEY IDENTITY(1,1),
+	name VARCHAR(100),
+	email VARCHAR(100),
+	password VARBINARY(150),
+	contactPhone CHAR(13),
+	fkHospital INT NOT NULL,
+	FOREIGN KEY (fkHospital) REFERENCES Hospital(_idHospital),
+	fkManager INT,
+	FOREIGN KEY (fkManager) REFERENCES UserHospital(_idUserHospital),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP 
 );
-
-
--- datasentry.dbo.Process definition
-
--- Drop table
-
--- DROP TABLE datasentry.dbo.Process;
-
-CREATE TABLE Process (
-	[_idProcess] int IDENTITY(1,1) NOT NULL,
-	name varchar(25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	createdAt datetime DEFAULT getdate() NULL,
-	updatedAt datetime DEFAULT getdate() NULL,
-	CONSTRAINT PK__Process__9492243694764CE9 PRIMARY KEY ([_idProcess])
+GO;
+CREATE TABLE Server(
+	_serialServer VARCHAR(50) PRIMARY KEY, -- senão pegar serial vamos pegar outro dado único do PC
+	isActive CHAR(1),
+	description VARCHAR(100),
+	fkHospital INT NOT NULL,
+	FOREIGN KEY (fkHospital) REFERENCES Hospital(_idHospital),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP 
 );
+GO;
 
-
--- datasentry.dbo.Server definition
-
--- Drop table
-
--- DROP TABLE datasentry.dbo.Server;
-
-CREATE TABLE Server (
-	[_serialServer] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	isActive char(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	description varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	fkHospital int NOT NULL,
-	createdAt datetime DEFAULT getdate() NULL,
-	updatedAt datetime DEFAULT getdate() NULL,
-	CONSTRAINT PK__Server__F59EF81ED7BFD8AC PRIMARY KEY ([_serialServer]),
-	CONSTRAINT FK__Server__fkHospit__4F47C5E3 FOREIGN KEY (fkHospital) REFERENCES Hospital([_idHospital])
+CREATE TABLE Process(
+	_idProcess INT PRIMARY KEY IDENTITY(1,1), -- talvez vire o PID
+	name VARCHAR(50),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP 
 );
-
-
--- datasentry.dbo.UserHospital definition
-
--- Drop table
-
--- DROP TABLE datasentry.dbo.UserHospital;
-
-CREATE TABLE UserHospital (
-	[_idUserHospital] int IDENTITY(1,1) NOT NULL,
-	name varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	email varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	password varbinary(150) NULL,
-	contactPhone char(13) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	fkHospital int NOT NULL,
-	fkManager int NULL,
-	createdAt datetime DEFAULT getdate() NULL,
-	updatedAt datetime DEFAULT getdate() NULL,
-	CONSTRAINT PK__UserHosp__963EB9DD311FB1F9 PRIMARY KEY ([_idUserHospital]),
-	CONSTRAINT FK__UserHospi__fkHos__498EEC8D FOREIGN KEY (fkHospital) REFERENCES Hospital([_idHospital]),
-	CONSTRAINT FK__UserHospi__fkMan__4A8310C6 FOREIGN KEY (fkManager) REFERENCES UserHospital([_idUserHospital])
+GO;
+CREATE TABLE ComponentType( 
+	_idComponentType INT PRIMARY KEY IDENTITY(1,1),
+	description VARCHAR(25), 
+	measuramentUnit VARCHAR(8),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP 
 );
-
-
--- datasentry.dbo.ComponentServer definition
-
--- Drop table
-
--- DROP TABLE datasentry.dbo.ComponentServer;
-
-CREATE TABLE ComponentServer (
-	[_idComponentServer] int IDENTITY(1,1) NOT NULL,
-	serial varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	model varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	brand varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	maxUse decimal(10,2) NULL,
-	fkServer varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	fkComponentType int NOT NULL,
-	createdAt datetime DEFAULT getdate() NULL,
-	updatedAt datetime DEFAULT getdate() NULL,
-	CONSTRAINT PK__Componen__1E87A23884F02A98 PRIMARY KEY ([_idComponentServer],fkComponentType),
-	CONSTRAINT FK__Component__fkCom__7849DB76 FOREIGN KEY (fkComponentType) REFERENCES ComponentType([_idComponentType]),
-	CONSTRAINT FK__Component__fkSer__7755B73D FOREIGN KEY (fkServer) REFERENCES Server([_serialServer])
+GO;
+CREATE TABLE ComponentServer(
+	_idComponentServer INT IDENTITY(1,1),
+	serial VARCHAR(60),
+	model VARCHAR(60),
+	brand VARCHAR(50),
+	maxUse DECIMAL(10,2),
+	fkServer VARCHAR(50) NOT NULL,
+	FOREIGN KEY (fkServer) REFERENCES Server(_serialServer),
+	fkComponentType INT NOT NULL,
+	FOREIGN KEY (fkComponentType) REFERENCES ComponentType(_idComponentType),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (_idComponentServer, fkComponentType)
 );
-
-
--- datasentry.dbo.LogComponentPerProcess definition
-
--- Drop table
-
--- DROP TABLE datasentry.dbo.LogComponentPerProcess;
-
-CREATE TABLE LogComponentPerProcess (
-	[_idLogComponentPerProcess] int IDENTITY(1,1) NOT NULL,
-	usageComponent decimal(10,2) NULL,
-	createdAt datetime DEFAULT getdate() NULL,
-	updatedAt datetime DEFAULT getdate() NULL,
-	fkProcess int NOT NULL,
-	fkComponentType int NOT NULL,
-	fkComponentServer int NOT NULL,
-	CONSTRAINT PK__LogCompo__40E184D17E0E322A PRIMARY KEY ([_idLogComponentPerProcess],fkComponentServer,fkComponentType,fkProcess),
-	CONSTRAINT FK__LogCompon__fkPro__7EF6D905 FOREIGN KEY (fkProcess) REFERENCES Process([_idProcess]),
-	CONSTRAINT FK__LogComponentPerP__7FEAFD3E FOREIGN KEY (fkComponentServer,fkComponentType) REFERENCES ComponentServer([_idComponentServer],fkComponentType)
+GO;
+CREATE TABLE LogComponentPerProcess(
+	_idLogComponentPerProcess INT IDENTITY(1,1),
+	usageComponent decimal(10,2),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	fkProcess INT,
+	FOREIGN KEY (fkProcess) REFERENCES Process(_idProcess),
+	fkComponentType INT,
+	fkComponentServer INT,
+	FOREIGN KEY (fkComponentServer, fkComponentType)
+	REFERENCES ComponentServer(_idComponentServer, fkComponentType),
+	PRIMARY KEY (_idLogComponentPerProcess, fkComponentServer, fkComponentType, fkProcess)
 );
+GO;
